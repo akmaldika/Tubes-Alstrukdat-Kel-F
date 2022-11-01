f46804c477383403a16fbda244728f3f59a39db7
zaydanA <16521131@std.stei.itb.ac.id>
2022-11-01 13:47:18 +0700

    addednewadt

---
+++ ADT_Makanan/makanan.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "boolean.h"
+#include "makanan.h"
+
+void CreateMakanan(MAKANAN *m, int id, char judul, __TIME__ exp,__TIME__ durasi,int loc){
+
+
+}
+
+int getId(MAKANAN m);
+
+char getJudul(MAKANAN m);
+
+__TIME__ getExp(MAKANAN m);
+
+__TIME__ getDurasi(MAKANAN *m);
+
+int getLoc(MAKANAN *m);
