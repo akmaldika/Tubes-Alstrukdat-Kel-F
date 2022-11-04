101f16bc4f4b8a5d7689c3e7cc57bdf2321bff8e
zaydanA <16521131@std.stei.itb.ac.id>
2022-11-04 21:22:13 +0700

    added driver

--- ADT_Matriks/driverMatrix.c
+++ ADT_Matriks/driverMatrix.c
@@ -3,12 +3,29 @@
 #include <math.h>
 #include "boolean.h"
 #include "matrix.c"
+#include <ctype.h>
+#include <stdlib.h>
 
 int main(){
     Matrix p;
     p=MapMatrix();
-    MoveWest(&p);
+    char input;
     displayMap(p);
+    while (input != 'b'){
+    
+        scanf("%c",&input); //gerakan menggunakan wasd
+        
+
+        system("cls");
+
+        displayMap(p);
+        ((int) tolower(input) == 'a') ? MoveWest(&p) :
+        ((int) tolower(input) == 'd') ? MoveEast(&p) :
+        ((int) tolower(input) == 'w') ? MoveNorth(&p) :
+        ((int) tolower(input) == 's') ? MoveSouth(&p) : "";
+
+    }
+
 
 
 }
