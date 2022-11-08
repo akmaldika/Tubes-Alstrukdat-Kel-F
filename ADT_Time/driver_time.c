9eb18bf8abf852a7d8b476c9b2e6fbe422b128e5
NerbSerg <13521043@mahasiswa.itb.ac.id>
2022-11-08 21:46:19 +0700

    update driver

--- ADT_Time/driver_time.c
+++ ADT_Time/driver_time.c
@@ -15,31 +15,47 @@ int main()
     DisplayTIME(b);
     printf("\n");
 
-    printf("\n a : ");
+    printf("\nNext min a : ");
     NextMin(&a);
     DisplayTIME(a);
+
     printf("\n");
+    printf("\nPrev min a : ");
     PrevMin(&a);
     DisplayTIME(a);
+
     printf("\n");
+    printf("\nPrev 5 min a : ");
     PrevNMin(&a, 5);
     DisplayTIME(a);
+
     printf("\n");
+    printf("\nNext 5 min a : ");
     NextNMin(&a, 5);
     DisplayTIME(a);
-    printf("\n");
-    setD(&a, 10);
-    DisplayTIME(a);
-    printf("\n");
 
-    NextMin(&a);
-    printf("Next a: ");
+    printf("\n");
+    printf("\nSet D 10 a : ");
+    setD(&a, 10);
+    DisplayTIMEk(a);
+
+    printf("\n\n");
+    printf("a : ");
     DisplayTIME(a);
     printf("\n");
-    PrevMin(&b);
-    printf("Prev b: ");
+    printf("b : ");
     DisplayTIME(b);
     printf("\n");
+    if (TGT(a, b))
+    {
+        printf("a is greater than b");
+    }
+    else
+    {
+        printf("a is not greater than b");
+    }
+
+    printf("\n\n");
 
     return 0;
 }
