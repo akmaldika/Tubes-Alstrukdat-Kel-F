b880ba963004d4b45cab9b91cf53f1c8d0598156
Fakhrimm <16521155@mahasiswa.itb.ac.id>
2022-11-17 06:19:37 +0700

    Update driver_cookbook for debugging test

--- Command/Driver_Cookbook.c
+++ Command/Driver_Cookbook.c
@@ -4,17 +4,20 @@
 gcc -lm -o a Driver_ListResep.c ListResep.c ..\ADT_Tree\NTree.c ..\ADT_MesinKata\mesinkata.c  ..\ADT_MesinKata\string.c ..\ADT_MesinKata\mesinkarakter.c ; ./a
 */
 int main() {
-    ListMakanan lm;
+    ListMakanan lm, lb;
     ListResep lr;
-    SetUpListMakanan("Driver_Config/Makanan.txt");
+    CreateListMakanan(&lm);
+    SetUpListMakanan(&lm, "Driver_Config/Makanan.txt");
     readListLR(&lr, "Driver_Config/Resep.txt");
     
     // printLR(lr);
-    
-    lm = DisplayActionAbleLM(ListMkn, "Fry");
     DisplayCatalog(lm);
+    
+    // lb = DisplayActionAbleLM(lm, "Fry");
+    // DisplayCatalog(lb);
     // COOKBOOKFOOD(lr, ListMkn);
-
+    printf("Selesai");
+    printf("halo");
     return 0;
 }
 /*gcc -lm -o a Driver_Cookbook.c  ..\ADT_Tree\NTree.c ..\ADT_MesinKata\mesinkata.c  ..\ADT_MesinKata\string.c ..\ADT_MesinKata\mesinkarakter.c ..\ADT_MultiSet\MultiSet.c ..\ADT_Makanan\makanan.c ..\ADT_ListStatik\ListMakanan.c ..\ADT_Time\time.c ..\ADT_ListStatik\ListResep.c ; ./a*/
