381689e7043781c95376fa10fcc2108d1306563b
4kmalm <16521070@mahasiswa.itb.ac.id>
2022-11-19 00:54:41 +0700

    Working on main

--- main.c
+++ main.c
@@ -1,45 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "library.h"
+#include "Command/splash.c"
+#include "Command/help.c"
+#include "Command/Exit.c"
+#include "Command/commandError.c"
+#include "Command/Cookbook.c"
+#include "Command/Move.c"
+#include "Command/Fryfood.c"
 
 int main()
 {
     /* KAMUS */
     ListResep ListRsp;
     ListMakanan ListMkn;
+    boolean isStartGame;
+    SIMULATOR BMO;
+    Stack Undo, Redo;
 
     /* ALGORITMA */
     /* Inisiasi program */
     SetUpListMakanan(&ListMkn, "Config/Config_Makanan.txt");
     readListLR(&ListRsp, "Confg/Config_Resep.txt");
-
-    // splashInitGame();
+    CreateEmptyStack(&Undo);
+    CreateEmptyStack(&Redo);
+    CreateSim(&BMO);
     
+
+    splashInitGame();
     STARTCOMMAND(); // currentWord
 
-    // Cek Start command error mcna nerima START sam EXIT
-        // loop sampai benar
+    // Cek Start command error mcna nerima START sam EXIT, loop sampai benar
+    commandStartError();
+    isStartGame = isLineSame(currentLine, "START");
 
-    // while bukan exit masuk loop
+    // while loop
+    while (isStartGame)
+    {
         // STARTCOMMAND semua yang diterima kecuali START
-        // cek benar salah 
-            // loop sampai benar
-            // cekcommand()
-        
-        // if else function 
-            // if command == <command>
-                // fungsi/prosedur command>
+        STARTCOMMAND();
+        // cek benar salah, loop sampai benar
+        commandInGameError();
 
-            // else if
-                // ...
-            // ...
+        if (isWordSame(currentWord, "HELP"))
+        {
+            helpInGame();
+        }
+        else if (isWordSame(currentWord, "BUY"))
+        {
+            //BUY
+        }
+        else if (isWordSame(currentWord, "MIX"))
+        {
+            //MIX
+        }
+        else if (isWordSame(currentWord, "CHOP"))
+        {
+            //CHOP
+        }
+        else if (isWordSame(currentWord, "FRY"))
+        {
+            FRYFOOD(&BMO, ListMkn, ListRsp, ) //************* KURANG FALG
+        }
+        else if (isWordSame(currentWord, "BOIL"))
+        {
+            //BOIL
+        }
+        else if (isWordSame(currentWord, "MOVE"))
+        {
+            //MOVE
+        }
+        else if (isWordSame(currentWord, "WAIT"))
+        {
+            //WAIT
+        }
+        else if (isWordSame(currentWord, "DELIVERY"))
+        {
+            //DELIVERY
+        }
+        else if (isWordSame(currentWord, "INVENTORY"))
+        {
+            //INVENTORY
+        }
+        else if (isWordSame(currentWord, "CATALOG"))
+        {
+            //CATALOG
+        }
+        else if (isWordSame(currentWord, "COOKBOOK"))
+        {
+            //COOKBOOK
+        }
+        else if (isWordSame(currentWord, "RECOMMENDATION"))
+        {
+            //RECOMMENDATION
+        }
+        else if (isWordSame(currentWord, "UNDO"))
+        {
+            //UNDO
+        }
+        else if (isWordSame(currentWord, "REDO"))
+        {
+            //REDO
+        }
+        else if (isWordSame(currentWord, "EXIT"))
+        {
+            isStartGame = false;
+        }
 
-            // if else.
-        
-        // ATUR NOTIF.
-    // until command = exit
+        // State baru masuk stak undo
+        // Atur Notif
+    }
+    // until command/currentLine = exit
 
     // splash exit
+    splashExitGame();
     // -program selesai-
 
     return 0;
