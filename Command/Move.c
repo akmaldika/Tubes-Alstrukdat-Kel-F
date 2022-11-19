886c8164d59221b19363b2fa739752f0e082c5d5
NerbSerg <13521043@mahasiswa.itb.ac.id>
2022-11-19 18:23:49 +0700

    Update Move.c

--- Command/Move.c
+++ Command/Move.c
@@ -3,79 +3,91 @@
 #include "../ADT_Point/point.h"
 #include "../ADT_Queue/PrioQueue.h"
 
-void MoveE(SIMULATOR *S){
-    Matrix m=MAP(*S);
-    TIME t=WAKTU(*S);
-    POINT p=TITIK(*S);
-    boolean sukses=false;
-    MoveEast(&m,&sukses) ;
-    if (sukses){
-        Geser(&p,1,0);
-        setLokasi(&S,p);
-        t=TPrevMin(t);
-        setTIme(&S,t);
-        setMap(&S,m);
-    }
-
-}
-
-void MoveW(SIMULATOR *S){
-    Matrix m=MAP(*S);
-    TIME t=WAKTU(*S);
-    POINT p=TITIK(*S);
-    boolean sukses=false;
-    MoveWest(&m,&sukses) ;
-    if (sukses){
-        Geser(&p,-1,0);
-        setLokasi(&S,p);
-        t=TPrevMin(t);
-        setTimeSim(&S,t);
-        setMap(&S,m);
-    }
-
-}
-
-void MoveN(SIMULATOR *S){
-    Matrix m=MAP(*S);
-    TIME t=WAKTU(*S);
-    POINT p=TITIK(*S);
-    boolean sukses=false;
-    MoveNorth(&m,&sukses) ;
-    if (sukses){
-    Geser(&p,0,-1);
-    setLokasi(&S,p);
-    t=TPrevMin(t);
-    setTimeSim(&S,t);
-    setMap(&S,m);
+void MoveE(SIMULATOR *S)
+{
+    Matrix m = MAP(*S);
+    TIME t = WAKTU(*S);
+    POINT p = TITIK(*S);
+    boolean sukses = false;
+    MoveEast(&m, &sukses);
+    if (sukses)
+    {
+        Geser(&p, 1, 0);
+        setTitikSim(S, p);
+        t = TPrevMin(t);
+        setTimeSim(S, t);
+        setMapSim(S, m);
     }
 }
 
-void MoveS(SIMULATOR *S){
-    Matrix m=MAP(*S);
-    TIME t=WAKTU(*S);
-    POINT p=TITIK(*S);
-    PrioQueue q=DELIV(*S);
-    boolean sukses=false;
-    MoveSouth(&m,&sukses) ;
-    if (sukses){
-    Geser(&p,0,1);
-    setLokasi(&S,p);
-    t=TPrevMin(t);
-    setTimeSim(&S,t);
-    setMap(&S,m);
+void MoveW(SIMULATOR *S)
+{
+    Matrix m = MAP(*S);
+    TIME t = WAKTU(*S);
+    POINT p = TITIK(*S);
+    boolean sukses = false;
+    MoveWest(&m, &sukses);
+    if (sukses)
+    {
+        Geser(&p, -1, 0);
+        setTitikSim(S, p);
+        t = TPrevMin(t);
+        setTimeSim(S, t);
+        setMapSim(S, m);
     }
 }
 
+void MoveN(SIMULATOR *S)
+{
+    Matrix m = MAP(*S);
+    TIME t = WAKTU(*S);
+    POINT p = TITIK(*S);
+    boolean sukses = false;
+    MoveNorth(&m, &sukses);
+    if (sukses)
+    {
+        Geser(&p, 0, -1);
+        setTitikSim(S, p);
+        t = TPrevMin(t);
+        setTimeSim(S, t);
+        setMapSim(S, m);
+    }
+}
 
+void MoveS(SIMULATOR *S)
+{
+    Matrix m = MAP(*S);
+    TIME t = WAKTU(*S);
+    POINT p = TITIK(*S);
+    PrioQueue q = DELIV(*S);
+    boolean sukses = false;
+    MoveSouth(&m, &sukses);
+    if (sukses)
+    {
+        Geser(&p, 0, 1);
+        setTitikSim(S, p);
+        t = TPrevMin(t);
+        setTimeSim(S, t);
+        setMapSim(S, m);
+    }
+}
 
-void Move(SIMULATOR *S, Word X){
-    if (isWordSame(X,'EAST')){
+void Move(SIMULATOR *S, Word X)
+{
+    if (isWordSame(X, "EAST"))
+    {
         MoveE(S);
-    }else if (isWordSame(X,'WEST')){
+    }
+    else if (isWordSame(X, "WEST"))
+    {
         MoveW(S);
-    }else if (isWordSame(X,'NORTH')){
+    }
+    else if (isWordSame(X, "NORTH"))
+    {
         MoveN(S);
-    }else if (isWordSame(X,'SOUTH')){
+    }
+    else if (isWordSame(X, "SOUTH"))
+    {
         MoveS(S);
     }
 }
