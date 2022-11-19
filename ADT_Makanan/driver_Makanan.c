fbf27d098f0fdff83296b5c8f8d7ab05062cb1f0
zaydanA <16521131@std.stei.itb.ac.id>
2022-11-20 03:07:09 +0700

    amtriks

--- ADT_Makanan/driver_Makanan.c
+++ ADT_Makanan/driver_Makanan.c
@@ -1,5 +1,12 @@
 #include "makanan.h"
 
 int main(){
-    
+    MAKANAN m;
+    IDEM id;
+    NamaMakanan namaMakanan;
+    TIME exp;
+    TIME deliv;
+    LokasiAksi loc;
+
+    CreateMakanan(&m, id, namaMakanan, exp, deliv, loc);
 }
