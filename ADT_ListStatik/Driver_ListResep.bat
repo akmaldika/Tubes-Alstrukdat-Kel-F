f010739efa759755ad2fd427f307c6b1c01f71ab
4kmalm <16521070@mahasiswa.itb.ac.id>
2022-11-20 11:45:49 +0700

    add..

---
+++ ADT_ListStatik/Driver_ListResep.bat
@@ -0,0 +1,1 @@
+gcc -lm -o Driver_ListResep Driver_ListResep.c ListResep.c ..\ADT_Tree\NTree.c ..\ADT_MesinKata\mesinkata.c  ..\ADT_MesinKata\string.c ..\ADT_MesinKata\mesinkarakter.c ..\ADT_MultiSet\MultiSet.c && Driver_ListResep
